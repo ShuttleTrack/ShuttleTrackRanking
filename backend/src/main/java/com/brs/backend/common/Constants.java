b43ed490c8d44d98a7679e4b0a35089c64e54b19
Amila <amilabanuka@gmail.com>
2025-02-25 21:30:53 +0100

    Reduced the demerit points

--- backend/src/main/java/com/brs/backend/common/Constants.java
+++ backend/src/main/java/com/brs/backend/common/Constants.java
@@ -1,8 +1,9 @@
 package com.brs.backend.common;
 
 public class Constants {
-    // Demerit points changed to -22 from -10 in Sep, 2024
-    public static final int DEMERIT_POINTS_ABSENTEE = -22;
+    // Demerit points changed to -22 from -10 in Sep, 2024,
+    // Demerit points changed to -20 from -22 in Feb 2025
+    public static final int DEMERIT_POINTS_ABSENTEE = -20;
     public static final int ABSENTEE_ENCOUNTER_ID = -1;
     public static final int DISABLE_PLAYER_ENCOUNTER_ID = -2;
     public static final int ACTIVATE_PLAYER_ENCOUNTER_ID = -3;
