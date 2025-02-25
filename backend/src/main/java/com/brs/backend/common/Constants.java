38173ddce457d12e10d9b2a044753ad07ede8b2e
Amila <amilabanuka@gmail.com>
2025-02-25 20:27:10 +0100

    Fixed constant value

--- backend/src/main/java/com/brs/backend/common/Constants.java
+++ backend/src/main/java/com/brs/backend/common/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
     public static final int ABSENTEE_ENCOUNTER_ID = -1;
     public static final int DISABLE_PLAYER_ENCOUNTER_ID = -2;
     public static final int ACTIVATE_PLAYER_ENCOUNTER_ID = -3;
-    public static final int COMPRESS_SCORE_ENCOUNTER_ID = -3;
+    public static final int COMPRESS_SCORE_ENCOUNTER_ID = -4;
 
 }
